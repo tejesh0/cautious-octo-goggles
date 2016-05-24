772f2049c4e59d830ffc01463b3afce3f97c769a
tejesh95 <tejeshpapineni95@gmail.com>
2016-05-24 10:33:55 +0530

    add static folder

--- twitch_project/twitch_project/settings.py
+++ twitch_project/twitch_project/settings.py
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'twitch_project/static/')
